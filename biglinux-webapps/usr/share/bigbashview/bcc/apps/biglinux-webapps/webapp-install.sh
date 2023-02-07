b5009124938b42152e1cb8556a54d92385b0329a
shadichy <hotrobb360web@gmail.com>
2023-02-08 00:27:26 +0700

    some stuffs

--- biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/webapp-install.sh
+++ biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/webapp-install.sh
@@ -7,42 +7,67 @@ _NAMEDESK=$(sed 's|https\:\/\/||;s|www\.||;s|\/.*||;s|\.|-|g' <<<"$urldesk")
 USER_DESKTOP=$(xdg-user-dir DESKTOP)
 
 NAME="${_NAMEDESK}-$RANDOM"
-BASENAME_APP="$NAME-webapp-biglinux-custom.desktop"
-LINK_APP="$HOME/.local/share/applications/$BASENAME_APP"
 DIR_PROF="$HOME/.bigwebapps/$NAME"
-FILE_LINK="$USER_DESKTOP/$BASENAME_APP"
+BASENAME="$NAME-webapp-biglinux-custom"
+BROWSER_NAME="${browser##*/}"
+case "$browser" in
+*epiphany)
+	BASENAME="$BROWSER_NAME.WebApp_$BASENAME"
+	DIR_PROF="$HOME/.bigwebapps/$BASENAME"
+	;;
+esac
+
+BASENAME_APP="$BASENAME.desktop"
+LINK_APP="$HOME/.local/share/applications/$BASENAME_APP"
+DESKTOP_LINK="$USER_DESKTOP/$BASENAME_APP"
+
+if [[ "$browser" = "$FLATPAK_BIN/"* ]]; then
+	DIR_PROF="$HOME/.var/app/$BROWSER_NAME/data/$NAME"
+	FLATPAK_LINE="X-Flatpak=$BROWSER_NAME"
+fi
 
 BASENAME_ICON="${icondesk##*/}"
 NAME_FILE="${BASENAME_ICON// /-}"
 ICON_FILE=~/.local/share/icons/"$NAME_FILE"
 
 if [[ "$urldesk" != *"://"* ]]; then
-    urldesk="https://$urldesk"
+	urldesk="https://$urldesk"
 fi
+
+gen_launcher() {
+	cat <<EOF >"$1"
+[Desktop Entry]
+Version=1.0
+Terminal=false
+Type=Application
+Name=$namedesk
+Exec=$2
+Icon=$ICON_FILE
+Categories=$category;
+StartupNotify=true
+X-KDE-StartupNotify=true
+$3
+$FLATPAK_LINE
+EOF
+}
+
+cp_icon() {
+	if [ "${icondesk##*/}" = "default-webapps.png" ]; then
+		cp "$icondesk" "$1"
+	else
+		mv "$icondesk" "$1"
+	fi
+}
 
 shopt -s nocasematch
 case "$browser" in
 *firefox | *firedragon | *librewolf*)
-    browser_name="$browser"
 
-    case "$browser" in
-    *.firefox | *.librewolf*)
-        if [ -x "$FLATPAK_BIN/$browser" ]; then
-            browser="$FLATPAK_BIN/$browser"
-            DIR_PROF="$HOME/.var/app/$browser_name/data/$NAME"
-        fi
-        ;;
-    esac
+	cp_icon "$ICON_FILE"
 
-    if [ "${icondesk##*/}" = "default-webapps.png" ]; then
-        cp "$icondesk" "$ICON_FILE"
-    else
-        mv "$icondesk" "$ICON_FILE"
-    fi
+	DESKBIN="$HOME/.local/bin/$NAME"
 
-    DESKBIN="$HOME/.local/bin/$NAME"
-
-    cat >"$DESKBIN" <<EOF
+	cat >"$DESKBIN" <<EOF
 #!/usr/bin/env sh
 #
 # Amofi - App mode for Firefox
@@ -72,154 +97,98 @@ case "$browser" in
 FOLDER=$DIR_PROF
 
 if ! grep -q 'toolkit.legacyUserProfileCustomizations.stylesheets' "\$FOLDER/prefs.js" 2>/dev/null;then
-    [ -d "\$FOLDER" ] && rm -r "\$FOLDER"
-    mkdir -p "\$FOLDER/chrome"
-    echo 'user_pref("media.eme.enabled", true);' >> "\$FOLDER/prefs.js"
-    echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> "\$FOLDER/prefs.js"
-    sed -i 's|user_pref("browser.urlbar.placeholderName.*||g' "\$FOLDER/prefs.js"
+	[ -d "\$FOLDER" ] && rm -r "\$FOLDER"
+	mkdir -p "\$FOLDER/chrome"
+	echo 'user_pref("media.eme.enabled", true);' >> "\$FOLDER/prefs.js"
+	echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> "\$FOLDER/prefs.js"
+	sed -i 's|user_pref("browser.urlbar.placeholderName.*||g' "\$FOLDER/prefs.js"
 
-    # Custom profile
-    echo '#nav-bar{visibility: collapse;} #TabsToolbar{visibility: collapse;}' >> "\$FOLDER/chrome/userChrome.css"
-    echo 'user_pref("browser.tabs.warnOnClose", false);' >> "\$FOLDER/user.js"
+	# Custom profile
+	echo '#nav-bar{visibility: collapse;} #TabsToolbar{visibility: collapse;}' >> "\$FOLDER/chrome/userChrome.css"
+	echo 'user_pref("browser.tabs.warnOnClose", false);' >> "\$FOLDER/user.js"
 fi
 
-CLASS="$browser_name-webapp-$_NAMEDESK"
+CLASS="$BROWSER_NAME-webapp-$_NAMEDESK"
 
 MOZ_DISABLE_GMP_SANDBOX=1 MOZ_DISABLE_CONTENT_SANDBOX=1 \
 $browser --class="\$CLASS" --profile "\$FOLDER" --no-remote --new-instance "$urldesk" &
 
 count=0
 while [ \$count -lt 100 ];do
-    wininfo=\$(xwininfo -root -children -all | grep \\"Navigator\\"\\ \\"\$CLASS\\")
-    if [ "\$wininfo" ];then
-        xseticon -id "\$(awk '{print \$1}' <<< \$wininfo)" $ICON_FILE
-        count=100
-    else
-        let count=count+1;
-    fi
-    sleep 0.5
+	wininfo=\$(xwininfo -root -children -all | grep \\"Navigator\\"\\ \\"\$CLASS\\")
+	if [ "\$wininfo" ];then
+		xseticon -id "\$(awk '{print \$1}' <<< \$wininfo)" $ICON_FILE
+		count=100
+	else
+		let count=count+1;
+	fi
+	sleep 0.5
 done
 EOF
 
-    chmod +x "$DESKBIN"
+	chmod +x "$DESKBIN"
 
-    echo "[Desktop Entry]
-Version=1.0
-Terminal=false
-Type=Application
-Name=$namedesk
-Exec=$DESKBIN
-Icon=$ICON_FILE
-Categories=$category;
-X-KDE-StartupNotify=true" >"$LINK_APP"
+	gen_launcher "$LINK_APP" "$DESKBIN"
+	;;
+*firefoxpwa)
 
-    chmod +x "$LINK_APP"
+	cp_icon "$ICON_FILE"
 
-    if [ "$shortcut" = "on" ]; then
-        ln -s "$LINK_APP" "$FILE_LINK"
-        chmod 755 "$FILE_LINK"
-        gio set "$FILE_LINK" -t string metadata::trust "true"
-    fi
-    ;;
+	FPWA_PROFILE="00000000000000000000000000"
+
+	APP_ID=$($browser site install --document-url "$urldesk" --icon-url "file://$ICON_FILE" --profile "$FPWA_PROFILE" --no-system-integration "data:application/manifest+json;base64,$(base64 <<<"{'start_url':'$urldesk','name':'$namedesk'}")" | tail -1 | awk '{print $6}')
+
+	gen_launcher "$LINK_APP" "$browser site launch --url $urldesk $APP_ID"
+	;;
+
 *epiphany)
-    case "$browser" in
-    *.Epiphany)
-        if [ -x "$FLATPAK_BIN/$browser" ]; then
-            DIR_PORTAL="$HOME/.local/share/xdg-desktop-portal"
-            DIR_PORTAL_APP="$DIR_PORTAL/applications"
-            DIR_PORTAL_ICON="$DIR_PORTAL/icons/64x64"
+	EPI_LINK="$DIR_PROF/$BASENAME_APP"
+	ICON_FILE="$DIR_PROF/app-icon.png"
 
-            mkdir -p "$DIR_PORTAL_APP"
-            mkdir -p "$DIR_PORTAL_ICON"
+	if [[ "$browser" = "$FLATPAK_BIN/"* ]]; then
+		DIR_PORTAL="$HOME/.local/share/xdg-desktop-portal"
+		DIR_PORTAL_APP="$DIR_PORTAL/applications"
+		DIR_PORTAL_ICON="$DIR_PORTAL/icons/64x64"
 
-            FOLDER_DATA="$HOME/.var/app/org.gnome.Epiphany/data/org.gnome.Epiphany.WebApp_$NAME-webapp-biglinux-custom"
-            browser="/var/lib/flatpak/exports/bin/org.gnome.Epiphany"
-            FLATPAK_LINE="X-Flatpak=org.gnome.Epiphany"
-            EPI_FILEDESK="org.gnome.Epiphany.WebApp_$NAME-webapp-biglinux-custom.desktop"
-            EPI_DIR_FILEDESK="$DIR_PORTAL_APP/$EPI_FILEDESK"
-            EPI_FILE_ICON="$DIR_PORTAL_ICON/${EPI_FILEDESK/.desktop/}.png"
-        fi
-        ;;
-    *)
-        FOLDER_DATA="$HOME/.bigwebapps/org.gnome.Epiphany.WebApp_$NAME-webapp-biglinux-custom"
-        FLATPAK_LINE=""
-        EPI_FILEDESK="org.gnome.Epiphany.WebApp_$NAME-webapp-biglinux-custom.desktop"
-        EPI_DIR_FILEDESK="$FOLDER_DATA/$EPI_FILEDESK"
-        EPI_FILE_ICON="$FOLDER_DATA/app-icon.png"
-        ;;
-    esac
+		mkdir -p "$DIR_PORTAL_APP"
+		mkdir -p "$DIR_PORTAL_ICON"
 
-    EPI_LINK="$HOME/.local/share/applications/$EPI_FILEDESK"
-    EPI_DESKTOP_LINK="$USER_DESKTOP/$EPI_FILEDESK"
-    mkdir -p "$FOLDER_DATA"
-    true >"$FOLDER_DATA/.app"
-    echo -n 37 >"$FOLDER_DATA/.migrated"
+		EPI_LINK="$DIR_PORTAL_APP/$BASENAME_APP"
+		ICON_FILE="$DIR_PORTAL_ICON/$BASENAME.png"
+	fi
 
-    if [ "${icondesk##*/}" = "default-webapps.png" ]; then
-        cp "$icondesk" "$EPI_FILE_ICON"
-    else
-        mv "$icondesk" "$EPI_FILE_ICON"
-    fi
+	DESKTOP_LINK="$USER_DESKTOP/$BASENAME_APP"
+	mkdir -p "$DIR_PROF"
+	true >"$DIR_PROF/.app"
+	echo -n 37 >"$DIR_PROF/.migrated"
 
-    echo "[Desktop Entry]
-Name=$namedesk
-Exec=$browser --application-mode --profile=$FOLDER_DATA $urldesk
-StartupNotify=true
-Terminal=false
-Type=Application
-Categories=$category;
-Icon=$EPI_FILE_ICON
-StartupWMClass=$namedesk
-X-Purism-FormFactor=Workstation;Mobile;
-$FLATPAK_LINE" >"$EPI_DIR_FILEDESK"
+	cp_icon "$ICON_FILE"
 
-    chmod +x "$EPI_DIR_FILEDESK"
-    ln -s "$EPI_DIR_FILEDESK" "$EPI_LINK"
+	gen_launcher "$EPI_LINK" "$browser --application-mode --profile=$DIR_PROF $urldesk" "X-Purism-FormFactor=Workstation;Mobile;"
 
-    if [ "$shortcut" = "on" ]; then
-        ln -s "$EPI_DIR_FILEDESK" "$EPI_DESKTOP_LINK"
-        chmod 755 "$EPI_DESKTOP_LINK"
-        gio set "$EPI_DESKTOP_LINK" -t string metadata::trust "true"
-    fi
-    ;;
+	ln -s "$EPI_LINK" "$LINK_APP"
+	;;
+
 *)
-    if [[ "$browser" = *"."* ]] && [ -x "$FLATPAK_BIN/$browser" ]; then
-        browser="$FLATPAK_BIN/$browser"
-        DIR_PROF="$HOME/.var/app/$browser_name/data/$NAME"
-    fi
+	if [ "$newperfil" = "on" ]; then
+		browser="$browser --user-data-dir=$DIR_PROF --no-first-run"
+	fi
 
-    if [ "$newperfil" = "on" ]; then
-        browser="$browser --user-data-dir=$DIR_PROF --no-first-run"
-    fi
+	cp_icon "$ICON_FILE"
 
-    if [ "${icondesk##*/}" = "default-webapps.png" ]; then
-        cp "$icondesk" "$ICON_FILE"
-    else
-        mv "$icondesk" "$ICON_FILE"
-    fi
+	CUT_HTTP=$(sed 's|https://||;s|/|_|g;s|_|__|1;s|_$||;s|_$||;s|&|_|g;s|?||g;s|=|_|g' <<<"$urldesk")
 
-    CUT_HTTP=$(sed 's|https://||;s|/|_|g;s|_|__|1;s|_$||;s|_$||;s|&|_|g;s|?||g;s|=|_|g' <<<"$urldesk")
-
-    echo "[Desktop Entry]
-Version=1.0
-Terminal=false
-Type=Application
-Name=$namedesk
-Exec=$browser --class=$CUT_HTTP,Chromium-browser --profile-directory=Default --app=$urldesk
-Icon=$ICON_FILE
-Categories=$category;
-StartupWMClass=$CUT_HTTP" >"$LINK_APP"
-
-    chmod +x "$LINK_APP"
-
-    if [ "$shortcut" = "on" ]; then
-        ln -s "$LINK_APP" "$FILE_LINK"
-        chmod 755 "$FILE_LINK"
-        gio set "$FILE_LINK" -t string metadata::trust "true"
-    fi
-    ;;
-
+	gen_launcher "$LINK_APP" "$browser --class=$CUT_HTTP,Chromium-browser --profile-directory=Default --app=$urldesk" "StartupWMClass=$CUT_HTTP"
+	;;
 esac
+
+chmod +x "$LINK_APP"
+
+if [ "$shortcut" = "on" ]; then
+	ln -s "$LINK_APP" "$DESKTOP_LINK"
+	chmod 755 "$DESKTOP_LINK"
+	gio set "$DESKTOP_LINK" -t string metadata::trust "true"
+fi
 
 nohup update-desktop-database -q ~/.local/share/applications &
 nohup kbuildsycoca5 &>/dev/null &
