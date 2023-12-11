6a2721c3ebd3f827888d934aec9694ae8d0840ba
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-12-11 14:03:08 -0300

    Update dynamicRender.js

--- biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/js/dynamicRender.js
+++ biglinux-webapps/usr/share/bigbashview/bcc/apps/biglinux-webapps/js/dynamicRender.js
@@ -17,10 +17,26 @@ function loadBrowsers(browserList) {
     const firstValue = firstOption.val()
     console.log("First-Browser-Combobox: " + firstValue)
     wrapper_browser(firstValue)
+
+    if (browserList.browsers.length === 0) {
+        $("#change-browser").innerHTML = `
+        <div class="pop-up__subtitle">
+          Não existem navegadores compatíveis instalados no sistema!
+        </div>
+        <div class="content-button-wrapper">
+          <button class="content-button status-button2 close">"Fechar"</button>
+        </div>`
+    }
+
+
 }
 
 window.addEventListener("DOMContentLoaded", async () => {
     await fetch("execute$python ./python/NavigatorList.py", { method: "GET" })
         .then(res => res.json())
-        .then(data => loadBrowsers(data))
+        .then(data => {
+            console.log("Loaded Browsers:")
+            console.log(data)
+            loadBrowsers(data)
+        })
 })
